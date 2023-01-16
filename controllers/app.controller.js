dbd2232f980c455831fad99ebd0603861d5fe974
Wiggy93 <110779323+Wiggy93@users.noreply.github.com>
2023-01-16 17:09:21 +0000

    added catch block to controller

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,9 +1,12 @@
 const {fetchCategories} = require("../models/app.model")
 
 const getCategory = (request, response, next) => {
-    fetchCategories().then((categories) => {
+    fetchCategories()
+    .then((categories) => {
         response.status(200).send({categories})
-    
+    })
+    .catch((err) => {
+        next(err);
     })
 }
 
