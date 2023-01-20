4b7207e23492eb00105e8c61045287fc05e26c8b
Wiggy93 <110779323+Wiggy93@users.noreply.github.com>
2023-01-20 17:03:32 +0000

    separating error handling in app to separate file

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const { getCategory, getReviews, getReviewById, getComments, patchVotes, getUsers, addComments, deleteComment} = require("../controllers/app.controller")
+const {customErrorHandler, stringInsteadOfNumber, notFound, missingFields, serverError} = require("./app.errors")
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/api/categories", getCategory)
 app.get("/api/reviews", getReviews)
@@ -20,41 +21,10 @@ app.all('*', (request, response) => {
     response.status(404).send({message : "Invalid path provided - please try again"})
 });
 
-app.use((err, req, res, next)=>{
-    if(err.status && err.message){
-        res.status(err.status).send({message : err.message});   
-        } else {
-        next(err);
-    }
-})
-
-app.use((err, req, res, next)=>{
-    if (err.code === "22P02"){
-        res.status(400).send({message : "Bad Request - expected a number and got text e.g. received three instead of 3"})
-    } else {
-        next(err)
-    }
-})
-
-app.use((err, req, res, next)=>{
-    if (err.code === "23503"){
-        res.status(404).send({message : "Not Found"})
-    } else {
-        next(err)
-    }
-})
-
-app.use((err, req, res, next)=>{
-    if (err.code === "23502"){
-        res.status(400).send({message : "Missing required fields in comment (username and/or comment)"})
-    } else {
-        next(err)
-    }
-})
-
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send({msg: "Internal server error"})
-})
+app.use(customErrorHandler)
+app.use(stringInsteadOfNumber)
+app.use(notFound)
+app.use(missingFields)
+app.use(serverError)
 
 module.exports = app;
